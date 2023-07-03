d31b02e0084675caf85f0726dbfa838d73a92557
Eloc <eloc@pudgypenguins.io>
2023-07-02 21:33:43 -0700

    square-sum

---
+++ square-sum.rs
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/515e271a311df0350d00000f/rust
+
+fn square_sum(vec: Vec<i32>) -> i32 {
+    let mut sum: i32 = 0;
+    for v in &vec{
+        sum += v*v
+    }
+    return sum
+}
